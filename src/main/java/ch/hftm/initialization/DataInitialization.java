b6211c2e6d36fd26916cd134da94b3b89671ff24
Alexander Stucker <alexander.stucker@hftm.ch>
2024-05-26 19:10:46 +0200

    - Implemented `DataInitialization` class in `ch.hftm.initialization` package:   - Initializes the database with sample blogs on application startup if the database is empty

---
+++ src/main/java/ch/hftm/initialization/DataInitialization.java
@@ -0,0 +1,27 @@
+package ch.hftm.initialization;
+
+import ch.hftm.entity.Blog;
+import ch.hftm.repository.BlogRepository;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+@ApplicationScoped
+public class DataInitialization {
+
+
+  @Inject
+  BlogRepository blogRepository;
+
+    @Transactional
+    public void init(@Observes StartupEvent event) {
+            if (blogRepository.count() == 0) {
+            Blog blog1 = new Blog("Titel", "Content");
+            Blog blog2 = new Blog("Titel 2", "Content2");
+            blogRepository.persist(blog1);
+            blogRepository.persist(blog2);
+    }
+  }
+}
