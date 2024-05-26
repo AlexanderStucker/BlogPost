6f73e1a722407a931fd057ed4a48563234b0588c
Alexander Stucker <alexander.stucker@hftm.ch>
2024-05-26 19:11:12 +0200

    - Changed `BlogRepository` in `ch.hftm.repository` package using PanacheRepository:   - Provides data access methods for `Blog` entity

---
+++ src/main/java/ch/hftm/repository/BlogRepository.java
@@ -0,0 +1,11 @@
+package ch.hftm.repository;
+
+import ch.hftm.entity.Blog;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class BlogRepository implements PanacheRepository<Blog> {
+    
+}
